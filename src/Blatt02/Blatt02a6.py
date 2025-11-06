5439fd6ad77dcf972bf6006749b1a5a5c51a4fce
MrTz06 <156386449+MrTz06@users.noreply.github.com>
2025-11-06 18:13:00 +0100

    Add matrix input and display to Blatt02a6.py
    Added user input for rows and columns, and implemented logic to print a matrix with 1s on the diagonal and 0s elsewhere. Also added new .idea/discord.xml configuration file.

--- src/Blatt02/Blatt02a6.py
+++ src/Blatt02/Blatt02a6.py
@@ -1,2 +1,13 @@
 # Gruppenmitglieder der Abgabegruppe 51:
 # Moritz Glaser(uk109727), Jordan Bank(uk110417), Daniel Bosman(uk107607)
+zeilen = int(input("Bitte geben sie eine Zahl ein: "))
+spalten = int(input("Bitte geben sie eine Zahl ein: "))
+
+for i in range(zeilen):
+    for j in range(spalten):
+        if(i==j):
+            print("1",end="")
+        else :
+            print("0",end="")
+
+    print()
